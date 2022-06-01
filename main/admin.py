76a8bf597289c6177b55ceeb0289234e38a8b098
Anton Troitski <93082658+NomadBYlife@users.noreply.github.com>
2022-06-01 21:23:14 +0200

    add models, admin

---
+++ main/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+
+from .models import CreditApplication, Contract, Product, Manufacturer
+
+
+@admin.register(CreditApplication)
+class CreditApplicationAdmin(admin.ModelAdmin):
+    """Кредитная заявка"""
+    list_display = ('id', 'contract', 'date')
+    list_display_links = ('contract',)
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    """Контракт"""
+    list_display = ('id', 'date')
+    list_display_links = ('id',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Товар"""
+    list_display = ('id', 'title', 'credit', 'manufacturer', 'date')
+    list_display_links = ('title',)
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    """Производитель"""
+    list_display = ('id', 'title', 'date')
+    list_display_links = ('title',)
---
+++ main/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+
+class CreditApplication(models.Model):
+    """Кредитная заявка"""
+    contract = models.OneToOneField("Contract", on_delete=models.CASCADE, related_name='contract',
+                                    verbose_name='контракт')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+
+    class Meta:
+        verbose_name = 'Заявку'
+        verbose_name_plural = 'Заявки'
+
+    def __str__(self):
+        return f'заявка номер {self.id}'
+
+
+class Contract(models.Model):
+    """Контракт"""
+    date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+
+    class Meta:
+        verbose_name = 'Контракт'
+        verbose_name_plural = 'Контракты'
+
+    def __str__(self):
+        return f'контракт номер {self.id}'
+
+
+class Product(models.Model):
+    """Товар"""
+    title = models.CharField(max_length=255, verbose_name='название')
+    credit = models.ForeignKey(CreditApplication, on_delete=models.CASCADE, related_name='credit',
+                               verbose_name='кредитная заявка')
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE, related_name='manufacturer',
+                                     verbose_name='производитель')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.title
+
+
+class Manufacturer(models.Model):
+    """Производитель"""
+    title = models.CharField(max_length=200, verbose_name='название')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+
+    class Meta:
+        verbose_name = 'Производителя'
+        verbose_name_plural = 'Производители'
+
+    def __str__(self):
+        return self.title
