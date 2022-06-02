363fa06874711f07347573ebd234ce218c1b5a9f
Anton Troitski <93082658+NomadBYlife@users.noreply.github.com>
2022-06-02 17:35:41 +0200

    add swagger

--- application/urls.py
+++ application/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .yasg import urlpatterns as swag_url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('', include('main.urls')),
     path('api/v1/', include('main.urlsAPI')),
 ]
+urlpatterns += swag_url
